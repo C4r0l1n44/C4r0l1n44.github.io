fa23f88433f3953a6251dea187df5d8c8d23dbd5
C4r0l1n44 <147450683+C4r0l1n44@users.noreply.github.com>
2024-03-21 02:29:15 -0500

    eklatweb

---
+++ JavaScript/inicio.js
@@ -0,0 +1,3 @@
+var personalizado;
+personalizado = prompt("¡Queremos saber mas sobre ti! Digite su nombre:")
+alert("¡Hola! " + personalizado + " Es un gusto estar en contacto contigo. Te damos la Bievenida")
